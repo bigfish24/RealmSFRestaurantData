7b7ea7ac593899df28244fee81d607a403c5eb94
Adam Fish <af@realm.io>
2015-06-10 17:18:53 -0700

    Fixed the function to retrieve the Realm file path to also move the file to the documents directory so that it has read/write access.

--- SFRestaurantScores.h
+++ SFRestaurantScores.h
@@ -13,10 +13,65 @@
 #import "ABFViolationObject.h"
 
 /**
+ *  Retrieve the path for a given file name in the documents directory
+ *
+ *  @return path to SFRestaurantScores.realm
+ */
+static NSString * ABFDocumentFilePathWithName(NSString *fileName)
+{
+#if TARGET_OS_IPHONE
+    // On iOS the Documents directory isn't user-visible, so put files there
+    NSString *path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+#else
+    // On OS X it is, so put files in Application Support. If we aren't running
+    // in a sandbox, put it in a subdirectory based on the bundle identifier
+    // to avoid accidentally sharing files between applications
+    NSString *path = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES)[0];
+    if (![[NSProcessInfo processInfo] environment][@"APP_SANDBOX_CONTAINER_ID"]) {
+        NSString *identifier = [[NSBundle mainBundle] bundleIdentifier];
+        if ([identifier length] == 0) {
+            identifier = [[[NSBundle mainBundle] executablePath] lastPathComponent];
+        }
+        path = [path stringByAppendingPathComponent:identifier];
+        
+        // create directory
+        [[NSFileManager defaultManager] createDirectoryAtPath:path
+                                  withIntermediateDirectories:YES
+                                                   attributes:nil
+                                                        error:nil];
+    }
+#endif
+    return [path stringByAppendingPathComponent:fileName];
+}
+
+/**
  *  Retrieve the path for the prebuilt SFRestaurantScores Realm file
  *
  *  @return path to SFRestaurantScores.realm
  */
 static NSString * ABFRestaurantScoresPath() {
-    return [[NSBundle mainBundle] pathForResource:@"SFRestaurantScores" ofType:@"realm"];
+    
+    // Move the file from bundle to the documents folder for read/write access
+    if (![[NSFileManager defaultManager] fileExistsAtPath:ABFDocumentFilePathWithName(@"SFRestaurantScores.realm")]) {
+        
+        NSString *fileInBundle = [[NSBundle mainBundle] pathForResource:@"SFRestaurantScores" ofType:@"realm"];
+        
+        NSString *fileInDocuments = ABFDocumentFilePathWithName(@"SFRestaurantScores.realm");
+        
+        NSError *error = nil;
+        
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        [fileManager copyItemAtPath:fileInBundle
+                             toPath:fileInDocuments
+                              error:&error];
+        
+        if (error) {
+            NSLog(@"Copy File Error: %@",error.localizedDescription);
+            
+            error = nil;
+        }
+    }
+    
+    return ABFDocumentFilePathWithName(@"SFRestaurantScores.realm");
 }
